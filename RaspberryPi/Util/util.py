8e111952e5fb375c01894c777c48fa384d40f754
aidenaltman <aiden.altman@gmail.com>
2025-04-15 17:49:55 -0400

    Removed unneeded files, util and camera

--- RaspberryPi/Util/util.py
+++
@@ -1,6 +0,0 @@
-#Axis that are registered as valid input
-validSticks = [5]
-validTriggers = [9, 10]
-
-#Keeps track of previous inputs sent by an ID to prevent serial clogging
-prevInstructions = {5:0.0, 9:0.0, 10:0.0}
