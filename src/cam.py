9ba4b789f3e382ba9eae05be8de55b107ab9547a
AlecLozano <lozanoalec46@gmail.com>
2025-04-17 23:42:05 +0100

    alec el dumbo

--- src/cam.py
+++
@@ -1,7 +0,0 @@
-from picamera2 import Picamera2
-import time
-
-picam = Picamera2(0)
-picam.start()
-time.sleep(2)
-picam.capture_file("test.jpg")
--- src/main.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo11s_edgetpu.tflite")
-results = model.predict('car.jpg')[0].boxes
-
-print(results)
