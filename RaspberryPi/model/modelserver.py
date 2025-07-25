2b65148db1ce640e98ed0925e77946381d2797af
AlecL <lozanoalec46@gmail.com>
2025-07-25 16:29:26 -0400

    fixed merge conflicts

--- RaspberryPi/model/modelserver.py
+++ RaspberryPi/model/modelserver.py
@@ -3,20 +3,6 @@ import socket
 import cv2
 import numpy as np
 import struct
-<<<<<<< HEAD
-
-HOST = ''  # Listen on all interfaces
-PORT = 9999
-
-def receive_image(conn):
-    # Receive 4 bytes first to know the length of the incoming image
-    data = b''
-    while len(data) < 4:
-        data += conn.recv(4 - len(data))
-    msg_len = struct.unpack('>I', data)[0]
-
-    # Receive the actual image data
-=======
 from ultralytics import YOLO
 
 import colordetect
@@ -38,7 +24,6 @@ def receive_frame(conn):
     msg_len = struct.unpack('>I', data)[0]
 
     # Receive the actual frame
->>>>>>> ca18cc3920c0082f20bbb33610832547dd6f9478
     data = b''
     while len(data) < msg_len:
         packet = conn.recv(msg_len - len(data))
@@ -46,40 +31,15 @@ def receive_frame(conn):
             return None
         data += packet
 
-<<<<<<< HEAD
-    # Decode image
-    np_data = np.frombuffer(data, dtype=np.uint8)
-    img = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
-    return img
-=======
     # Decode JPEG bytes to image
     frame_data = np.frombuffer(data, dtype=np.uint8)
     frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
     return frame
->>>>>>> ca18cc3920c0082f20bbb33610832547dd6f9478
 
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen(1)
-<<<<<<< HEAD
-        print(f"Server listening on port {PORT}...")
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            img = receive_image(conn)
-
-            if img is not None:
-                cv2.imshow('Received Image', img)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                conn.sendall(b"Image received successfully.")
-            else:
-                print("Failed to receive image.")
-
-if __name__ == "__main__":
-    start_server()
-=======
         print(f"[SERVER] Listening on port {PORT}...")
         conn, addr = s.accept()
         print("[SERVER] Connected by", addr)
@@ -116,5 +76,4 @@ if __name__ == "__main__":
             start_server()
         except ConnectionResetError:
             print("Client Connection closed")
->>>>>>> ca18cc3920c0082f20bbb33610832547dd6f9478
 
