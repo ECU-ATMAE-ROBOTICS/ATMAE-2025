cbb7e8c5e020e67c2cd1d02d34f1007cdb633dbd
AlecLozano <lozanoalec46@gmail.com>
2025-08-26 15:00:23 -0400

    Robot follows bins

--- RaspberryPi/server/direct.py
+++ RaspberryPi/server/direct.py
@@ -1,62 +1,57 @@
 import math
 
-def determine_direction(img, colors, boxes, reference,threshold):
+def determine_direction(img, boxes):
     """
     Determine the direction a robot should move to align with a target bin
     based on YOLO-detected bounding boxes and tape colors.
 
     Args:
         img (numpy.ndarray): Image from the camera (BGR format).
-        colors (list): List of detected tape colors associated with bounding boxes.
         boxes (list): List of bounding box coordinates in the format [x0, y0, x1, y1].
-        reference (str): The target color of the box to align with.
-        threshold (int): Threshold value representing the tolerance around the 
-            middle section of the image for alignment.
+
 
     Returns:
         str: A movement command string formatted as:
-            - "9:-1\n" if the bot is close enough and aligned with the target bin.
             - "5:{amount_to_turn}\n" if the bot needs to turn, where `amount_to_turn`
               is a float between -1.0 and 1.0.
     """
     #Get center of image
     img_height, img_width = img.shape[:2]
     img_center = [img_height//2, img_width//2]
-    target_bin = None
-
-
-    try:
-        #Get bin to align with
-        target_bin = boxes[colors.index(reference)]
-    
-    #If the target box is not in view, go in the direction of detected boxes
-    except:
-        target_bin = boxes[0]
-        
-        
+    target_bin = boxes[0]
 
     bin_height, bin_width = [target_bin[3]-target_bin[1], target_bin[2]-target_bin[0]]
     bin_center = [target_bin[3]-bin_height//2, target_bin[2]-bin_width//2]
 
-    #Get percent box takes up in image
-    image_per = img_height*2 + img_width*2
-    bin_per = bin_height*2 + bin_width*2
+    amount_to_turn = (bin_center[1]/img_center[1] -1)
+
+    #Account for right drift
+    if amount_to_turn > 0:
+        amount_to_turn = 1
+
+    turn = f"5:{amount_to_turn:.2f}\n" # Turn
     
-    percent_covered = (bin_per/image_per) * 100
+    return turn
 
-    #print("image perimeter: ",image_per)
-    #print("box perimeter: ", bin_per)
-
-    #print("percent covered: ", percent_covered)
-
-    #Stop the bot if it's close enough and aligned
-    if percent_covered > 40 and bin_center[1] < img_center[1]+60 and bin_center[1] > img_center[1]-60:
-        return "9:-1\n"
+def determine_throttle(img, boxes):
     
-    #Siulate controller turn input based on how far from center bin is
+    #Get center of image
+    img_height, img_width = img.shape[:2]
+    img_center = [img_height//2, img_width//2]
+    target_bin = boxes[0]
+
+    bin_height, bin_width = [target_bin[3]-target_bin[1], target_bin[2]-target_bin[0]]
+    bin_center = [target_bin[3]-bin_height//2, target_bin[2]-bin_width//2]
+
+    percent_covered =  ((bin_height*2 + bin_width*2)/(img_height*2 + img_width*2)) * 100 #percent of image covered by bin
+    print(percent_covered)
+
+    if percent_covered > 23:
+        return f"9:-1\n"
     else:
-        amount_to_turn = (bin_center[1]/img_center[1] -1) * .65
-        return f"5:{amount_to_turn:.2f}\n"
+        return f"9:-.6\n"
+
+
 
 
 
--- RaspberryPi/server/modelserver.py
+++ RaspberryPi/server/modelserver.py
@@ -88,7 +88,8 @@ def start_server():
                     
                     while True:
                         frame = receive_frame(conn)
-                        direction = "None"
+                        messages = ""
+
                         if frame is None:
                             break
                         #frame = cv2.resize(frame, (448,448))
@@ -100,24 +101,25 @@ def start_server():
                             colors = colordetect.get_box_colors(frame, boxes)
                             colors,boxes = colordetect.sort_bins(colors,boxes)
                             
-                            # ----Colored bin to track ("blue") is hardcoded right now, will be automatically determined in the future-----
-                            direction = direct.determine_direction(frame, colors, boxes, colors[0], 40)
+                            direction = direct.determine_direction(frame, boxes)
+                            throttle = direct.determine_throttle(frame, boxes)
+
+                            messages = f"{direction}|{throttle}"
                         
                         # Comment this out if you dont need visualization
                             for indx, box in enumerate(boxes):
                                 cv2.rectangle(frame, box[:2], box[2:], (0,0,255), 2)
                             
-                            frame = cv2.putText(frame, direction[:-1], (20,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                            frame = cv2.putText(frame, messages, (20,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
                         
 
                         # Display the received frame
                         cv2.imshow("Live Feed", frame)
                         if cv2.waitKey(1) == 27:  # Press 'ESC' to stop
                             break
-                    
-
-                        # Send acknowledgment back to client
-                        conn.sendall(direction.encode())
+                        
+                        #print(fr"{messages}")
+                        conn.sendall(messages.encode())
                 cv2.destroyAllWindows()
                 print("[SERVER] Connection closed.")
             except KeyboardInterrupt:
