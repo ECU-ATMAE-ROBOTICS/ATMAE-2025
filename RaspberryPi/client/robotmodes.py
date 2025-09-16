0cec225865e96ba2f9dec101d3e405e2c78a7cb4
aidenaltman <aiden.altman@gmail.com>
2025-09-16 17:29:30 -0400

    feat: implement sorting mode to trigger TBD when goal reached

--- RaspberryPi/client/robotmodes.py
+++ RaspberryPi/client/robotmodes.py
@@ -6,6 +6,7 @@ import struct
 from datetime import datetime
 import threading
 import queue
+import RaspberryPi.server.internalsort as internalsort 
 
 SERVER_IP = '192.168.4.32'  # Change to the IP of the server
 PORT = 9999
@@ -186,3 +187,44 @@ def auto(controller, arduino):
 
         time.sleep(.2)
 
+
+
+def internal_sort_mode(arduino):
+
+    """
+    Run the robot in internal sorting mode.
+
+    Continuously captures frames from the camera, processes them to
+    detect ball colors, and sends corresponding servo commands to
+    the Arduino. Stops after sorting 12 balls.
+
+    Args:
+        arduino (serial.Serial): Serial connection object to the Arduino.
+
+    Returns:
+        None
+    """
+
+    cap = cv2.VideoCapture(1)
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        color = internalsort.internal_sort(frame, sep_color=None)
+
+        if color is not None:
+            arduino.write(f"{color}:1\n".encode("utf-8"))
+            print(f"Sent command: {color}")
+
+        if color == 4:  # Assuming 4 indicates completion of sorting 12 balls
+            break
+        
+        if color == 5:  # Jiggle command to dislodge stuck balls
+            pass # I dont really know what this would look like
+
+
+        time.sleep(0.5)  # Adjust delay as needed
+
+    cap.release()
